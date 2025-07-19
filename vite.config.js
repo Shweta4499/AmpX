1a21b6a200f6aa4e79e022c38cc5e58348095698
Shweta Gurav <shwetagurav4499@gmail.com>
2025-07-19 17:13:02 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line no-undef
+  base:process.env.VITE_BASE_PATH ||"/AmpX",
 });
